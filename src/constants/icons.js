420c1367c6db5e08ebe829658978d4d9e7bf171e
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-03-19 16:26:27 +0100

    fresh psushshsh

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,37 +1,6 @@
 export const about = require('../assets/icons/about.png')
-export const account = require('../assets/icons/account.png')
-export const arrowleft = require('../assets/icons/arrowleft.png')
-export const arrowright = require('../assets/icons/arrowright.png')
-export const check = require('../assets/icons/check.png')
-export const close = require('../assets/icons/close.png')
-export const explore = require('../assets/icons/explore.png')
-export const eyeclose = require('../assets/icons/eyeclose.png')
-export const eyeopen = require('../assets/icons/eyeopen.png')
-export const inbox = require('../assets/icons/inbox.png')
-export const location = require('../assets/icons/location.png')
-export const lookup = require('../assets/icons/lookup.png')
-export const padlock = require('../assets/icons/padlock.png')
-export const person = require('../assets/icons/person.png')
-export const search = require('../assets/icons/search.png')
-export const shop = require('../assets/icons/shop.png')
-export const topchef = require('../assets/icons/topchef.png')
 
 export default {
     about,
-    account,
-    arrowleft,
-    arrowright,
-    check,
-    close,
-    explore,
-    eyeclose,
-    eyeopen,
-    inbox,
-    location,
-    lookup,
-    padlock,
-    person,
-    search,
-    shop,
-    topchef,
+
 }
