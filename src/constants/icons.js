56fa6c4855283bc1ee0dd50c1805a6c9fec94fee
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-03-19 16:59:02 +0100

    icons fixing

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,6 +1,37 @@
 export const about = require('../assets/icons/about.png')
+export const apple = require('../assets/icons/apple.png')
+export const arrowleft = require('../assets/icons/arrowleft.png')
+export const arrowleft2 = require('../assets/icons/arrowleft2.png')
+export const arrowright = require('../assets/icons/arrowright.png')
+export const eyeclose = require('../assets/icons/eyeclose.png')
+export const facebook = require('../assets/icons/facebook.png')
+export const game = require('../assets/icons/game.png')
+export const google = require('../assets/icons/google.png')
+export const home = require('../assets/icons/home.png')
+export const horizontalmenu = require('../assets/icons/horizontalmenu.png')
+export const message = require('../assets/icons/message.png')
+export const more = require('../assets/icons/more.png')
+export const notification = require('../assets/icons/notification.png')
+export const person = require('../assets/icons/person.png')
+export const search = require('../assets/icons/search.png')
+export const verticalmenu = require('../assets/icons/verticalmenu.png')
 
 export default {
     about,
-
+    apple,
+    arrowleft,
+    arrowleft2,
+    arrowright,
+    eyeclose,
+    facebook,
+    game,
+    google,
+    home,
+    horizontalmenu,
+    message,
+    more,
+    notification,
+    person,
+    search,
+    verticalmenu,
 }
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,13 @@
+export const logo1 = require('../assets/images/logo1.png')
 export const nigeriaflag = require('../assets/images/nigeriaflag.png')
+export const slide1 = require('../assets/images/slide1.png')
+export const slide2 = require('../assets/images/slide2.png')
+export const slide3 = require('../assets/images/slide3.png')
 
 export default {
+    logo1,
     nigeriaflag,
+    slide1,
+    slide2,
+    slide3,
 }
