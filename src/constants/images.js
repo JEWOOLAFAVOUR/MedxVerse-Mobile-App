f490fbcddf61d2b8cedf020227bcfd2832af7536
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-03-29 19:41:53 +0100

    working on it cool

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,4 +1,5 @@
 export const avatar1 = require('../assets/images/avatar1.png')
+export const avatar2 = require('../assets/images/avatar2.png')
 export const logo1 = require('../assets/images/logo1.png')
 export const logo2 = require('../assets/images/logo2.png')
 export const nigeriaflag = require('../assets/images/nigeriaflag.png')
@@ -9,6 +10,7 @@ export const successful = require('../assets/images/successful.png')
 
 export default {
     avatar1,
+    avatar2,
     logo1,
     logo2,
     nigeriaflag,
--- src/screens/Main/More/MoreScreen.js
+++ src/screens/Main/More/MoreScreen.js
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { COLORS, SIZES, images } from '../../../constants'
 
 const MoreScreen = () => {
     return (
-        <View>
-            <Text>MoreScreen</Text>
+        <View style={styles.page}>
+            <View>
+                <Image source={images.avatar1} style={{ height: SIZES.h1 * 2, width: SIZES.h1 * 2, }} />
+            </View>
         </View>
     )
 }
 
 export default MoreScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+    },
+});
