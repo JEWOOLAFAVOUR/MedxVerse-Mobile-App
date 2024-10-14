bee65bf8883c3252213676a07155a449ef70460e
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-10-14 12:06:37 +0100

    working on the frontend, cool

--- src/redux/constants/index.js
+++ src/redux/constants/index.js
@@ -1,7 +1,7 @@
 import { DarkTheme, DefaultTheme } from "@react-navigation/native"
 // ./gradlew assembleRelease
 export default {
-    asyncStorageKey: "Suru001",
-    BASE_URL: 'https://suru-backends.onrender.com/',
-    // BASE_URL: "jjdjd",
+    asyncStorageKey: "medxlearn",
+    // BASE_URL: "http://192.168.6.67:8000/api/v1/"",
+    BASE_URL: "https://medxlearn.onrender.com/api/v1",
 }
